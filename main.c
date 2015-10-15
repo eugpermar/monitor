03716bcdf749280a315b140aa65d2ce90a46fbe2
Ana Rey <anarey@redborder.net>
2015-10-15 14:02:32 +0000

    Fixed leak memory: Delete pthread_cancel.

--- main.c
+++ main.c
@@ -1357,7 +1357,6 @@ int main(int argc, char  *argv[])
 			Log(LOG_INFO,"Leaving, wait for workers...\n");
 
 			for(int i=0;i<main_info.threads;++i){
-				pthread_cancel(pd_thread[i]);
 				pthread_join(pd_thread[i], NULL);
 			}
 			free(pd_thread);
